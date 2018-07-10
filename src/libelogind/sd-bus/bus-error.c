66278d8d0ab68dbcf3b96ac67bd81508d6130a48
Chen Qi <Qi.Chen@windriver.com>
2018-07-10 15:40:17 +0800

    distinguish XSI-compliant strerror_r from GNU-specifi strerror_r
    XSI-compliant strerror_r and GNU-specifi strerror_r are different.
    
           int strerror_r(int errnum, char *buf, size_t buflen);
                       /* XSI-compliant */
    
           char *strerror_r(int errnum, char *buf, size_t buflen);
                       /* GNU-specific */
    
    We need to distinguish between them. Otherwise, we'll get an int value
    assigned to (char *) variable, resulting in segment fault.
    
    Upstream-Status: Inappropriate [musl specific]
    
    Signed-off-by: Chen Qi <Qi.Chen@windriver.com>
    Signed-off-by: Achill Gilgenast <achill@achill.org>

--- src/libelogind/sd-bus/bus-error.c
+++ src/libelogind/sd-bus/bus-error.c
@@ -410,7 +410,12 @@ static void bus_error_strerror(sd_bus_error *e, int error) {
                         return;
 
                 errno = 0;
+#ifndef __GLIBC__
+                strerror_r(error, m, k);
+                x = m;
+#else
                 x = strerror_r(error, m, k);
+#endif
                 if (errno == ERANGE || strlen(x) >= k - 1) {
                         free(m);
                         k *= 2;
@@ -595,8 +600,12 @@ const char* _bus_error_message(const sd_bus_error *e, int error, char buf[static
 
         if (e && e->message)
                 return e->message;
-
+#ifndef __GLIBC__
+        strerror_r(abs(error), buf, ERRNO_BUF_LEN);
+        return buf;
+#else
         return strerror_r(abs(error), buf, ERRNO_BUF_LEN);
+#endif
 }
 
 static bool map_ok(const sd_bus_error_map *map) {
